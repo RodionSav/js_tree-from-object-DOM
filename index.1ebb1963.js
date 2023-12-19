3de622b2fec210f29b4ba8c52b4a03eea03cdf12
Rodionsav <rodionsav@mate.academy>
2023-12-20 00:22:11 +0100

    make build

---
+++ index.1ebb1963.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=document.createElement("ul");for(let t in n){let l=document.createElement("li");l.textContent=t,Object.keys(n[t]).length>0&&e(l,n[t]),r.append(l)}return t.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.1ebb1963.js.map
